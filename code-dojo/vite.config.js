7c4ba74cee629bafc8ac4379c031f2f52a533c0c
eldaduz <eldaduz@gmail.com>
2026-03-10 00:32:46 +0200

    sync: update code-dojo from private repo

--- code-dojo/vite.config.js
+++ code-dojo/vite.config.js
@@ -9,18 +9,4 @@ export default defineConfig({
     setupFiles: './src/test/setup.js',
     css: true,
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('firebase')) return 'firebase'
-            if (id.includes('@codemirror') || id.includes('react-codemirror')) return 'codemirror'
-            if (id.includes('react')) return 'react'
-            return 'vendor'
-          }
-        },
-      },
-    },
-  },
 })
